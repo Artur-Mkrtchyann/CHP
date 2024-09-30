5b12cbee4c30d3c692cc07c936880defd8c93721
Artur Mkrtchyann <art.mk2016@bk.ru>
2024-09-30 22:57:23 +0400

    lab 2.cpp

--- lab 2.cpp
+++ lab 2.cpp
@@ -1,25 +1,60 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
-int main() {
-    string mixed = "qwertyuiopasdfghjklzxcvbnm"; 
-    string text, result;
-
-    cout << "Greq text: ";
-    getline(cin, text); 
-
+string encrypt(const string& text, const string& mixed) {
+    string result;
     for (char c : text) {
-        if (isalpha(c)) { 
+        if (isalpha(c)) {
             char encryptedChar = mixed[tolower(c) - 'a']; 
             result += (c >= 'A' && c <= 'Z') ? (encryptedChar - 'a' + 'A') : encryptedChar;
         } else {
             result += c; 
         }
     }
+    return result;
+}
 
-    cout << "Gaxtnagrvac text: " << result << endl; 
+string decrypt(const string& text, const string& mixed) {
+    string result;
+    unordered_map<char, char> reverseMap;
+    
+    for (int i = 0; i < 26; ++i) {
+        reverseMap[mixed[i]] = 'a' + i;
+        reverseMap[toupper(mixed[i])] = 'A' + i;
+    }
+    
+    for (char c : text) {
+        if (isalpha(c)) {
+            result += reverseMap[c];
+        } else {
+            result += c; 
+        }
+    }
+    return result;
+}
+
+int main() {
+    string mixed = "qwertyuiopasdfghjklzxcvbnm"; 
+    string text, encryptedText, decryptedText;
+
+    cout << "Greq text Gaxtnagrman hamar: ";
+    getline(cin, text);
+
+    encryptedText = encrypt(text, mixed);
+    cout << "Gaxtnagrvac text: " << encryptedText << endl;
+
+    
+    decryptedText = decrypt(encryptedText, mixed);
+    cout << "Vercanvac text: " << decryptedText << endl;
+
+    if (text == decryptedText) {
+        cout << "Decrypt katarvel e: Skzbanakan texty nuynn e inj vercanvacy." << endl;
+    } else {
+        cout << "error: chi hamynknum texty." << endl;
+    }
 
     return 0;
 }
