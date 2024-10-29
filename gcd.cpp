831d81c39a218462882c37817cef90587cc6394d
Artur Mkrtchyann <art.mk2016@bk.ru>
2024-10-29 09:42:52 +0400

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -3,13 +3,11 @@
 using namespace std;
 
 int gcd(int a, int b) {
-
     while (b != 0) {
         int temp = a % b;
         a = b;
         b = temp;
     }
-
     return (a < 0) ? -a : a;
 }
 
@@ -18,17 +16,22 @@ int bazmapatik(int a, int b) {
 }
 
 int main() {
-
     int a, b;
 
     cout << "Enter two numbers: ";
     cin >> a >> b;
 
-    int res = bazmapatik(a, b);
-    int result = gcd(a, b);
-    cout<< "bajanarar:"<< result<<endl;
 
-    cout << "bazmapatik: " << res;
+    if ((a > 0 && b < 0) || (a < 0 && b > 0)) {
+        cout << "Goyutyun chuni amenapoqr yndhanur bazmapatik" << endl;
+        return 1; 
+    }
+
+    int result = gcd(a, b);
+    int res = bazmapatik(a, b);
+
+    cout << "bajanarar: " << result << endl;
+    cout << "bazmapatik: " << res << endl;
 
     return 0;
 }
